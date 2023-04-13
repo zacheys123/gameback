9a693d4c5ce76a0392d377f5795fdd3e6c13d226
zachey <12keysjams@gmail.com>
2023-04-13 16:44:05 +0300

    all ew

--- controllers/adminLogin.js
+++ controllers/adminLogin.js
@@ -2,7 +2,7 @@ import User from '../models/user.js';
 import jwt from 'jsonwebtoken';
 export const adminLogin = async (req, res) => {
 	const { username, secret, secret_question } = req.body;
-	console.log(username);
+	console.log(req.body);
 	try {
 		if (username && secret && secret_question) {
 			const result = await User.findOne({
@@ -11,25 +11,33 @@ export const adminLogin = async (req, res) => {
 				secret_question,
 			});
 
-			if (!result) {
-				res
-					.status(404)
-					.json({ message: 'Sorry,You are Not An Admin' });
+			if (username !== result?.username) {
+				res.status(404).json({
+					message: 'Sorry,Username does not match our database',
+				});
+			} else if (secret !== result?.secret) {
+				res.status(404).json({
+					message: 'Sorry,secret does not match our database',
+				});
+			} else if (secret_question !== result?.secret_question) {
+				res.status(404).json({
+					message: 'Sorry,secret query does not match our database',
+				});
+			} else {
+				return jwt.sign(
+					{ username: result.username, id: result._id },
+					process.env.JWT_SECRET,
+					{ expiresIn: process.env.JWT_EXPIRE },
+					(err, token) => {
+						res.status(200).json({
+							success: true,
+							message: `Welcome ${username}`,
+							result,
+							token,
+						});
+					},
+				);
 			}
-
-			return jwt.sign(
-				{ username: result.username, id: result._id },
-				process.env.JWT_SECRET,
-				{ expiresIn: process.env.JWT_EXPIRE },
-				(err, token) => {
-					res.status(200).json({
-						success: true,
-						message: `Welcome ${username}`,
-						result,
-						token,
-					});
-				},
-			);
 		} else {
 			return res.status(400).json({
 				success: false,
